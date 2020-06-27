35eb69c1c439dbb883ffd920ebc98484b4abe007
Ronak <parkhiya.r@husky.neu.edu>
2020-06-27 14:19:28 -0400

    Backend APIs server skeleton

---
+++ backend-server/server.js
@@ -0,0 +1,31 @@
+let express = require('express'),
+    app = express(), 
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.connect('mongodb://localhost:27017/todoBackendServer', {});
+mongoose.Promise = global.Promise; //Global is implicit object in nodejs, where you can use implicit objects
+
+//Adding body parser for handling request and response objects.
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+//Enabling CORS (Cross origin request sharing) // tell the server to run even if request come from different domain
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//const routes = require('./app/routes/index');
+//routes(app);
+
+// const initApp = require('./app/app');
+// initApp(app);
+
+app.listen(port);
+console.log('Todo RESTful API server started on: ' + port);
--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import axios from 'axios';
 
 
 export default {
@@ -21,23 +22,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "todo 1",
-          completed: false 
-        },
-        {
-          id: 2,
-          title: "todo 2",
-          completed: true 
-        },
-        {
-          id: 3,
-          title: "todo 3",
-          completed: false 
-        }
-      ]
+      todos: []
     }
   },
   methods: {
@@ -46,6 +31,11 @@ export default {
     },
     addTodo(newTodo){
       this.todos = [...this.todos, newTodo]
+    },
+    created() {
+      axios.get('https://jsonplaceholder.typicode.com/todos')
+        .then(res => this.todos = res.data)
+        .catch(err => console.log(err));
     }
   }
 }
